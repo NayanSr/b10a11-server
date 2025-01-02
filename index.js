b6614ccad0dc8a28d4cc33aadd5dce3ab3343bc1
Nayan Suter <nayankbps@gmail.com>
2025-01-02 21:48:06 +0600

    set basic mongodb connection and inserte one data to database

--- index.js
+++ index.js
@@ -12,6 +12,50 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+//! mongodb starts
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yo4en.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    const artifactsCollection= client.db("artifactsDB").collection("allArtifacts");
+
+    app.post('/allArtifacts', async(req,res)=>{
+        const data= req.body;
+        const result= await artifactsCollection.insertOne(data);
+        console.log('heated', data);
+        res.send(result)
+    })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+//! mongodb ends
+
+
+
+
 // Define routes
 app.get('/', async (req, res) => {
     res.send('Running this server');
