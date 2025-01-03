c15ab55a118051b2337a0ff6d8407a507c3bd600
Nayan Suter <nayankbps@gmail.com>
2025-01-03 21:38:20 +0600

    getting single artifact

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 //! mongodb starts
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yo4en.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,12 +36,32 @@ async function run() {
 
     const artifactsCollection= client.db("artifactsDB").collection("allArtifacts");
 
-    //! GET
+  //! GET
+
+        //? All artifacts
     app.get('/allArtifacts', async(req,res)=>{
         const cursor= artifactsCollection.find();
         const result= await cursor.toArray();
         res.send(result)
     })
+
+
+        //? single Data
+    app.get(`/allArtifacts/:id`, async(req,res)=>{
+      const id= req.params.id;
+      const query= {_id: new ObjectId(id)};
+      const result=await artifactsCollection.findOne(query);
+      res.send(result)
+      // console.log(result);
+    })
+
+
+        //? top six liked
+    app.get('/top-six', async(req,res)=>{
+      const result = await artifactsCollection.find({}).sort({like:-1}).limit(3).toArray();
+      res.send(result)
+    })
+      
     
     //! POST 
     app.post('/allArtifacts', async(req,res)=>{
@@ -74,3 +94,10 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+
+
+
+
+
+// TODO: top six liked
